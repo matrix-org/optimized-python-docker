7e130b6a9d316901014872ed4f727a5518e380a9
Amber H. Brown <hawkowl@atleastfornow.net>
2019-07-17 02:51:38 +1000

    fix to use march

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -euox pipefail
 LTO=1 OPTIMAL=1 MARCH=skylake-avx512 PYTHON_VERSION=3.7.4 make build-image
 
 # Output it, so we can upload it on a trusted machine
-docker save matrixdotorg/optimised-python:3.7.4-optimized-lto | gzip -9 > docker.tar.gz
+docker save matrixdotorg/optimised-python:3.7.4-optimized-lto-skylake-avx512 | gzip -9 > docker.tar.gz
